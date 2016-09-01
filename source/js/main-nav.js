21ac79fe67cb62d45f8fc771d0a8934a7f721ad9
Alexander Gurov <Alexander Gurov>
2016-09-01 19:09:58 +0300

    bug fix for page navigation

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -6,6 +6,7 @@ var $pageHeaderMenu = $('.page-header__menu');
 var $window = $(window);
 var $hamburgerIcon = $('.page-header__hamburger-ico');
 var $mainNav = $('.main-nav');
+var $mobileNav = $('.page-header__mobile');
 
 // Check if element in the view trigger skillbar animation
 function menuOnScroll () {
@@ -26,12 +27,27 @@ $('.main-nav__link').on('click', function(e) {
   e.preventDefault();
   var href = $(this).attr('href');
 
-  $('html body').animate({
-    scrollTop: $(href).offset().top - $pageHeaderMenu.outerHeight()
-  }, 1000);
-
-  $mainNav.slideUp();
+  if($window.innerWidth() <= 768) {
+    $('html body').animate({
+      scrollTop: $(href).offset().top - ($pageHeaderMenu.outerHeight() - $mainNav.outerHeight())
+    }, 1000, function() {
+      $mainNav.slideUp();
+    });
+  } else {
+    $('html body').animate({
+      scrollTop: $(href).offset().top - $pageHeaderMenu.outerHeight()
+    }, 1000);
+  }
 });
+
+$(window).resize(function(){
+  var width = $(window).width();
+
+  if (width > 768 && $mainNav.is(':hidden')) {
+    $mainNav.removeAttr('style');
+  }
+});
+
 
 $window.on('scroll resize', menuOnScroll);
 $window.trigger('scroll');
